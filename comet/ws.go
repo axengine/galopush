6d4a7980a24078ac95ac53e7bc3bf5a24c6d74a9
axengine <axengine@163.com>
2016-08-17 09:50:30 +0800

    websocket 使用加密传输

--- comet/ws.go
+++ comet/ws.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/json"
+	//	"encoding/json"
 	"galopush/internal/logs"
 	"galopush/internal/protocol"
 	"net/http"
@@ -44,86 +44,12 @@ func (p *Comet) handlerWsconn(conn *websocket.Conn) {
 			return
 		}
 		logs.Logger.Debug("ws receive ", string(buf))
-		if err = p.unMarshal(conn, buf); err != nil {
+
+		msg, err := protocol.UnPackJson(buf)
+		if err != nil {
 			logs.Logger.Error("protocol.DecodeJson error=", err, " buf=", string(buf[:]))
 			return
 		}
+		p.Cache(conn, msg)
 	}
-}
-
-//unMarshal 将json字符串解析并转换为二进制结构，并发送到数据处理channel
-//让业务处理模块能对tcp/ws一致性处理
-//返回值:error
-func (p *Comet) unMarshal(conn *websocket.Conn, buffer []byte) error {
-	var body map[string]interface{}
-	if err := json.Unmarshal(buffer, &body); err != nil {
-		logs.Logger.Error(err, " msg=", string(buffer))
-		return err
-	}
-	cmd := int(body["cmd"].(float64))
-	tid := int(body["tid"].(float64))
-	data := body["data"].(map[string]interface{})
-	switch cmd {
-	case protocol.MSGTYPE_REGISTER:
-		{
-			var msg protocol.Register
-			protocol.SetMsgType(&msg.Header, cmd)
-			protocol.SetEncode(&msg.Header, 0)
-			msg.Tid = uint32(tid)
-			msg.Len = 66
-
-			//消息体
-			msg.Version = byte(data["version"].(float64))
-			msg.TerminalType = byte(data["termType"].(float64))
-			idBuff := []byte(data["id"].(string))
-			for i := 0; i < 32 && i < len(idBuff); i++ {
-				msg.Id[i] = idBuff[i]
-			}
-			tokenBuff := []byte(data["token"].(string))
-			for i := 0; i < 32 && i < len(tokenBuff); i++ {
-				msg.Token[i] = tokenBuff[i]
-			}
-			p.Cache(conn, &msg)
-		}
-	case protocol.MSGTYPE_HEARTBEAT:
-		{
-			var msg protocol.Header
-			//固定头
-			protocol.SetMsgType(&msg, cmd)
-			protocol.SetEncode(&msg, 0)
-			msg.Tid = uint32(tid)
-			p.Cache(conn, &msg)
-		}
-
-	case protocol.MSGTYPE_PUSHRESP, protocol.MSGTYPE_CBRESP, protocol.MSGTYPE_MSGRESP, protocol.MSGTYPE_KICKRESP:
-		{
-			var msg protocol.Resp
-			//固定头
-			protocol.SetMsgType(&msg.Header, cmd)
-			protocol.SetEncode(&msg.Header, 0)
-			msg.Tid = uint32(tid)
-
-			//可变头
-			msg.Len = 1
-			msg.Code = byte(data["code"].(float64))
-			p.Cache(conn, &msg)
-		}
-	case protocol.MSGTYPE_MESSAGE:
-		var msg protocol.ImUp
-		//固定头
-		protocol.SetMsgType(&msg.Header, cmd)
-		protocol.SetEncode(&msg.Header, 0)
-		msg.Tid = uint32(tid)
-
-		content := data["msg"].(string)
-		contentBuff := []byte(content)
-
-		msg.Len = uint32(len(contentBuff))
-
-		msg.Msg = append(msg.Msg, contentBuff...)
-
-		p.Cache(conn, &msg)
-	}
-
-	return nil
 }
--- internal/protocol/json.go
+++ internal/protocol/json.go
@@ -1,8 +1,9 @@
 package protocol
 
 import (
-	//	"encoding/base64"
+	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"galopush/internal/logs"
 )
 
@@ -66,10 +67,107 @@ func packJson(v interface{}) []byte {
 		}
 	}
 	logs.Logger.Debug("packJson=", string(buf[:]))
-	return buf
+	return []byte(Encode(buf))
 }
 
 func marshal(data interface{}) ([]byte, error) {
 	b, err := json.Marshal(data)
 	return b, err
+}
+
+func Encode(buff []byte) string {
+	CodecEncode(buff, len(buff), ENCODE_LOOP_XOR)
+	return base64.StdEncoding.EncodeToString(buff)
+}
+
+func Decode(buff []byte) ([]byte, error) {
+	b, err := base64.StdEncoding.DecodeString(string(buff))
+	if err != nil {
+		logs.Logger.Error("Decode ws on decode base64 error ", err)
+		return b, err
+	}
+	CodecDecode(b, len(b), ENCODE_LOOP_XOR)
+	return b, nil
+}
+
+func UnPackJson(buffer []byte) (interface{}, error) {
+	b, err := Decode(buffer)
+	if err != nil {
+		return nil, err
+	}
+	var body map[string]interface{}
+	if err := json.Unmarshal(b, &body); err != nil {
+		logs.Logger.Error(err, " msg=", string(b))
+		return nil, err
+	}
+	cmd := int(body["cmd"].(float64))
+	tid := int(body["tid"].(float64))
+	data := body["data"].(map[string]interface{})
+	switch cmd {
+	case MSGTYPE_REGISTER:
+		{
+			var msg Register
+			SetMsgType(&msg.Header, cmd)
+			SetEncode(&msg.Header, 0)
+			msg.Tid = uint32(tid)
+			msg.Len = 66
+
+			//消息体
+			msg.Version = byte(data["version"].(float64))
+			msg.TerminalType = byte(data["termType"].(float64))
+			idBuff := []byte(data["id"].(string))
+			for i := 0; i < 32 && i < len(idBuff); i++ {
+				msg.Id[i] = idBuff[i]
+			}
+			tokenBuff := []byte(data["token"].(string))
+			for i := 0; i < 32 && i < len(tokenBuff); i++ {
+				msg.Token[i] = tokenBuff[i]
+			}
+			//p.Cache(conn, &msg)
+			return &msg, nil
+		}
+	case MSGTYPE_HEARTBEAT:
+		{
+			var msg Header
+			//固定头
+			SetMsgType(&msg, cmd)
+			SetEncode(&msg, 0)
+			msg.Tid = uint32(tid)
+			//p.Cache(conn, &msg)
+			return &msg, nil
+		}
+
+	case MSGTYPE_PUSHRESP, MSGTYPE_CBRESP, MSGTYPE_MSGRESP, MSGTYPE_KICKRESP:
+		{
+			var msg Resp
+			//固定头
+			SetMsgType(&msg.Header, cmd)
+			SetEncode(&msg.Header, 0)
+			msg.Tid = uint32(tid)
+
+			//可变头
+			msg.Len = 1
+			msg.Code = byte(data["code"].(float64))
+			//p.Cache(conn, &msg)
+			return &msg, nil
+		}
+	case MSGTYPE_MESSAGE:
+		var msg ImUp
+		//固定头
+		SetMsgType(&msg.Header, cmd)
+		SetEncode(&msg.Header, 0)
+		msg.Tid = uint32(tid)
+
+		content := data["msg"].(string)
+		contentBuff := []byte(content)
+
+		msg.Len = uint32(len(contentBuff))
+
+		msg.Msg = append(msg.Msg, contentBuff...)
+
+		//p.Cache(conn, &msg)
+		return &msg, nil
+	}
+
+	return nil, errors.New("UnKnow cmd type")
 }
